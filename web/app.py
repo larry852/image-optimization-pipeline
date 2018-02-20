626497a8c0dd98d6bb11f0a884428a119275d123
larry852 <larryportocarrero@gmail.com>
2018-02-19 22:34:22 -0500

    Subida de imagen

--- web/app.py
+++ web/app.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template, request, url_for, redirect
 from os import listdir
-from os.path import isfile, join
-import imghdr
+from os.path import join
+
+UPLOAD_FOLDER = 'static/img/input/'
+ALLOWED_EXTENSIONS = set(['rgb', 'gif', 'pbm', 'pgm', 'ppm', 'tiff', 'rast', 'xbm', 'jpeg', 'bmp', 'png', 'webp', 'exr', 'jpg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/')
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    images_path = 'static/img/input/'
-    images = [join(images_path, file) for file in listdir(images_path) if isfile(join(images_path, file)) and imghdr.what(join(images_path, file))]
+    images = [(join(app.config['UPLOAD_FOLDER'], file), file.split('.')[0]) for file in listdir(app.config['UPLOAD_FOLDER']) if allowed_file(file)]
+    if request.method == 'POST':
+        new_file = request.files['file']
+        if new_file and allowed_file(new_file.filename):
+            filename = str(len(images) + 1) + '.' + new_file.filename.rsplit('.', 1)[1].lower()
+            new_file.save(join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
     return render_template('index.html', images=images)
 
 
