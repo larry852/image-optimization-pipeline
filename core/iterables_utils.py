6b26fbfe0812955432b0ec82f680f9db8b2f4844
larry852 <larryportocarrero@gmail.com>
2018-04-07 19:39:52 -0500

    No listo for return

--- core/iterables_utils.py
+++ core/iterables_utils.py
@@ -5,19 +5,18 @@ import memory
 
 
 def powerset_list(iterable):
-    return list(chain.from_iterable(combinations(iterable, r) for r in range(len(iterable) + 1)))
+    return chain.from_iterable(combinations(iterable, r) for r in range(len(iterable) + 1))
 
 
 def permutations_list(iterable):
-    return list(permutations(iterable))
+    return permutations(iterable)
 
 
 if __name__ == '__main__':
     memory.memory_limit()
     try:
         time = default_timer()
-        # transformations = ['remove_mean', 'standardize', 'contrast_adjust', 'flip_lr', 'flip_ud', 'flip_lr_ud', 'image_pad', 'text_binarizarion', 'gaussian_blur', 'low_brightness_negative', 'edge_detection', 'enhance_basic_color', 'enhance_basic_contrast', 'enhance_basic_brightness', 'enhance_basic_sharpness', 'negative', 'intensity_increase', 'logarithmic_transformation', 'exponential_transformation', 'binarization', 'gray_fractionation', 'histogram_equalization', 'grayscale', 'posterize', 'solarize', 'remove_noise', 'clean_imagemagic', 'crop_morphology']
-        transformations = ['histogram_equalization', 'grayscale', 'posterize', 'solarize', 'remove_noise', 'clean_imagemagic', 'crop_morphology', 'remove_mean', 'gaussian_blur', 'contrast_adjust']
+        transformations = ['remove_mean', 'standardize', 'contrast_adjust', 'flip_lr', 'flip_ud', 'flip_lr_ud', 'image_pad', 'text_binarizarion', 'gaussian_blur', 'low_brightness_negative', 'edge_detection', 'enhance_basic_color', 'enhance_basic_contrast', 'enhance_basic_brightness', 'enhance_basic_sharpness', 'negative', 'intensity_increase', 'logarithmic_transformation', 'exponential_transformation', 'binarization', 'gray_fractionation', 'histogram_equalization', 'grayscale', 'posterize', 'solarize', 'remove_noise', 'clean_imagemagic', 'crop_morphology']
 
         print()
         print('---------------------------------------Permutations---------------------------------------')
@@ -35,8 +34,6 @@ if __name__ == '__main__':
 
         print()
         print('------------------------------------------------------------------------------------------')
-        print("Total powerset:" + str(len(powerset_result)))
-        print("Total permutations:" + str(len(permutations_result)))
         print("Total time execution: " + str(time_end))
     except MemoryError:
         sys.stderr.write('\n\nERROR: Memory Exception\n')
