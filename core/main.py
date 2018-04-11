065c55415713856811aca03feca7ea0a26f1234d
larry852 <larryportocarrero@gmail.com>
2018-04-11 12:04:42 -0500

    Test pipeline

--- core/main.py
+++ core/main.py
@@ -80,13 +80,18 @@ def transformation(name, image):
     elif name == 'crop_morphology':
         return transformations.crop_morphology(image)
     else:
+        print('default')
         return image
 
 
 def pipeline(filepath, steps):
     image = load_image(filepath)
+    path_temp = 'static/img/temp/current.png'
     for step in steps:
+        print(image)
         image = transformation(step, image)
+        save_image(image, path_temp)
+        image = load_image(path_temp)
 
     filename = '-'.join(steps) + '-' + str(uuid.uuid4()).split('-')[0]
     save_image(image, 'static/img/pipelines/{}.png'.format(filename))
@@ -162,6 +167,8 @@ if __name__ == '__main__':
         from . import iterables_utils
     except Exception:
         import iterables_utils
+
+    # individual('/home/larry/image-optimization-pipeline/static/img/input/1.jpg')
 
     list_transformations = ['remove_mean', 'standardize', 'contrast_adjust', 'flip_lr', 'flip_ud', 'flip_lr_ud', 'image_pad', 'text_binarizarion', 'gaussian_blur', 'low_brightness_negative', 'edge_detection', 'enhance_basic_color', 'enhance_basic_contrast', 'enhance_basic_brightness', 'enhance_basic_sharpness', 'negative', 'intensity_increase', 'logarithmic_transformation', 'exponential_transformation', 'binarization', 'gray_fractionation', 'histogram_equalization', 'grayscale', 'posterize', 'solarize', 'remove_noise', 'clean_imagemagic', 'crop_morphology']
     permutations = iterables_utils.get_permutations(list_transformations)
