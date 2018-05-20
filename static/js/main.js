eaeb86705a3b7c412bfdf966769ab409c2066acf
larry852 <larryportocarrero@gmail.com>
2018-05-20 18:30:31 -0500

    Style list of trasnformations

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,11 @@
 $('input[type="checkbox"]').bind('click', function() {
     selected = [];
     $('#selectTransformations').html('');
-
     $('input:checked').each(function() {
         selected.push($(this).attr('value'));
     });
-
     for (var i = selected.length - 1; i >= 0; i--) {
-	    var li = $('<li>');
-	    li.text(selected[i]).appendTo('#selectTransformations');
-    }    
+        var li = $('<li>');
+        li.text(selected[i]).appendTo('#selectTransformations');
+    }
 });
