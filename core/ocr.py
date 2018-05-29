ffd6ed82fd706b605027d3fb7181b3881c38ca13
larry852 <larryportocarrero@gmail.com>
2018-05-29 15:07:45 -0500

    Refactoring ocr compare

--- core/ocr.py
+++ core/ocr.py
@@ -3,12 +3,11 @@ import pytesseract
 from fuzzywuzzy import fuzz
 
 
-def compare(original_path, result_path):
-    result = Image.open(result_path)
-    original_text = 'LUIS ALEJANDRO ALENCASTRO CIFUENTES'
+def compare(text, path):
+    result = Image.open(path)
     result_text = pytesseract.image_to_string(result, lang='spa')
-    return original_text, result_text, fuzz.token_set_ratio(original_text, result_text)
+    return result_text, fuzz.token_set_ratio(text, result_text)
 
 
 if __name__ == '__main__':
-    print(compare('/home/larry/image-optimization-pipeline/static/img/input/2.jpg', '/home/larry/image-optimization-pipeline/static/img/pipelines/results/1-ccc3dcab.png'))
+    print(compare('LUIS ALEJANDRO ALENCASTRO CIFUENTES', '/home/larry/image-optimization-pipeline/static/img/pipelines/results/1-ccc3dcab.png'))
