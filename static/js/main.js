19c4d24887244d0170713247c40eeb3b8fea9b9e
larry852 <larryportocarrero@gmail.com>
2018-08-24 09:44:31 -0500

    js function

--- static/js/main.js
+++ static/js/main.js
@@ -76,3 +76,17 @@ function ocr_table_steps(results) {
     });
     container.append(table);
 }
+
+function ocr_table(results) {
+    var container = $('#ocr-table');
+    container.html('');
+    table = $('<table class="table table-bordered"><thead class="thead-dark"><tr><th>#</th><th>Texto original</th><th>Texto resultado</th><th>%</th></tr></thead>');
+    results.forEach(function(result) {
+        var tr = $('<tr>');
+        ['pipeline', 'original', 'result', 'percentage'].forEach(function(attr) {
+            tr.append('<td>' + result[attr] + '</td>');
+        });
+        table.append(tr);
+    });
+    container.append(table);
+}
